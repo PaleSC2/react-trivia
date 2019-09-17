ee854104aed0352d0ff3c39c1a7b59a2ab202509
Palecio <palecio@xumak.com>
2019-09-17 17:56:50 -0600

    sdf

--- data.js
+++ data.js
@@ -1,46 +1,91 @@
 var data = {
-  title: 'Alexander Von Humboldt',
+  title: 'Alexander Von Humboldt Jeopardy',
   description:
     'The Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.',
-  button: 'The Game',
-  player1: 'Player 1',
-  player2: 'Player 2',
-  points: 'points',
-  turn: 'turn',
+  button: 'Spiel',
+  player1: 'Spieler 1',
+  player2: 'Spieler 2',
+  points: 'punkte',
+  turn: 'dran sein',
   winnerMessage: 'is the winner!',
   restartButton: 'play again',
   game: [
     {
-      category: 'ES 2015',
+      category: 'FAKTEN',
       questions: [
         {
           points: 100,
           question:
-            'Constants (const) are:',
-          options: ['Block scoped', 'Function scoped', 'Global'],
-          answer: 0
+            'Mit wem war Alexander V.H. befreundet?',
+          options: ['David Hume', 'Johann Wolfgang Goethe', 'Gottfried Wilhelm Leibniz', 'Jean Jacques Rousseau'],
+          answer: 1
         },
+        {
+          points: 200,
+          question:
+            'Wer begleitete Alexander V.H. auf seiner Südamerikaneise?',
+          options: ['Der Ureinwohner Freitag', 'Sein Bruder', 'Der Entdecker Marco Polo', 'Der Botaniker Aimé Bonpland'],
+          answer: 3
+        },
+        {
+          points: 300,
+          question:
+            'Wer hat finanziert Alexander V.H. Reise nach Amerika?',
+          options: ['Er ebte nach dem Tod seiner Mutter', 'Er gewann in der königlichen Lotterie', 'Er bekam eine hohe Prämie für seine Entwicklung einer Grubenlampe', 'Er spekulierte geschicht mit Aktien'],
+          answer: 0
+        }
+      ]
+    },
+    {
+      category: 'LEBEN',
+      questions: [
         {
           points: 100,
           question:
-            'Constants (const) are:',
-          options: ['Block scoped', 'Function scoped', 'Global'],
-          answer: 0
+            'Wo und wann Alexander von Humboldt geboren wurde?',
+          options: ['In Frankfurt, 14. September 1769', 'In Berlin, 14. September 1789', 'In Berlin, 14. Oktober 1769', 'In Berlin, 1. September 1769'],
+          answer: 1
         },
+        {
+          points: 200,
+          question:
+            'Wo und wann Alexander von Humboldt gestorben ist',
+          options: ['In Berlin, 6. Mai 1900', 'In Berlin, 6. Juli 1859', 'In Frankfurt, 6. Mai 1859', 'In Berlin, 6. Mai 1859'],
+          answer: 3
+        },
+        {
+          points: 300,
+          question:
+            'Welche amerikanischen Länder haben Sie während Ihrer Reise besucht?',
+          options: ['Mexiko, Venezuela, Kolumbien, Ecuador, Peru, USA', 'Mexiko, Venezuela, Guatemala, Ecuador, Peru, USA', 'Argentinien, Venezuela, Kolumbien, Ecuador, Peru', 'Brassilien, Venezuela, Ecuador, Peru, USA'],
+          answer: 0
+        }
+      ]
+    },
+    {
+      category: 'ARBEIT',
+      questions: [
         {
           points: 100,
           question:
-            'Constants (const) are:',
-          options: ['Block scoped', 'Function scoped', 'Global'],
-          answer: 0
+            'Alexander von Humboldt schrieb en Gesamtschau der wissenschaftlichen Welterforschung. Unter welchem Title erschien das fünfbändige Werk?',
+          options: ['Universum', 'All', 'Kosmo', 'Sphäre'],
+          answer: 2
         },
         {
-          points: 100,
+          points: 200,
           question:
-            'Constants (const) are:',
-          options: ['Block scoped', 'Function scoped', 'Global'],
-          answer: 0
+            'Nach dem Studium verschidener Disziplinen arbeitete Alexander von humboldt zunächst im Staatsdienst. In welchem Bereich?',
+          options: ['Recht', 'Medizin', 'Bergbau', 'Politik'],
+          answer: 2
         },
+        {
+          points: 300,
+          question:
+            'In welchem der folgender Romane ist Alexander von Humboldt einer der Protagonisten?',
+          options: ['Aldous Huxley: Schöne neue Welt', 'Daniel Defoe: Robinson Crusoe', 'Naomi J. Williams: Die letzten Entdecker', 'Daniel Kehlmann: Die Vermessung der Welt'],
+          answer: 3
+        }
       ]
     }
   ]
